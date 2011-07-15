bd4e57bd2a2c2591b243f673c654051c8e81f32f
Sebastian Wiesinger <sebastian@karotte.org>
2011-07-15 21:57:16 +0200

    Display subalbums in albumlist.py

--- albumlist.py
+++ albumlist.py
@@ -16,6 +16,23 @@ FLAGS.AddFlag('h', 'hostname', 'The hostname to use', 'localhost')
 FLAGS.AddFlag('t', 'table_prefix', 'The table prefix to use', 'g2_')
 FLAGS.AddFlag('f', 'field_prefix', 'The field prefix to use', 'g_')
 
+def printAlbum(album, subalbums, photos, level=0):
+
+    print level*'\t' + '%s' %  album
+
+    try:
+      for sub in subalbums[album.id()]:
+        printAlbum(sub, subalbums, photos, level+1)
+    except KeyError:
+      pass
+
+    try:
+      for photo in photos[album.id()]:
+        print level*'\t' + '\t%s' % photo
+    except KeyError:
+      pass
+
+
 def main(argv):
   appname = argv[0]
 
@@ -29,10 +46,19 @@ def main(argv):
       FLAGS.hostname, FLAGS.table_prefix, FLAGS.field_prefix)
 
   try:
+    rootalbum = None
     albums = []
     album_ids = gdb.ItemIdsForTable(items.AlbumItem.TABLE_NAME)
+    subalbums_by_album = {}
     for id in album_ids:
+      album = items.AlbumItem(gdb,id)
       albums.append(items.AlbumItem(gdb, id))
+      if album.parent_id():
+        if album.parent_id() not in subalbums_by_album:
+          subalbums_by_album[album.parent_id()] = []
+        subalbums_by_album[album.parent_id()].append(album)
+      else:
+        rootalbum = album
 
     photos_by_album = {}
     photo_ids = gdb.ItemIdsForTable(items.PhotoItem.TABLE_NAME)
@@ -43,14 +69,8 @@ def main(argv):
 
       photos_by_album[photo.parent_id()].append(photo)
 
-    for album in albums:
-      print album
+    printAlbum(rootalbum, subalbums_by_album, photos_by_album)
 
-      if album.id() not in photos_by_album:
-        continue
-
-      for photo in photos_by_album[album.id()]:
-        print '\t%s' % photo
   finally:
     gdb.close()
 
